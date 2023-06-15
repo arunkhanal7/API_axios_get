a8f4ce601a993750908a6b39a6fceaaa373320ce
Arun <kingarun2020@gmail.com>
2023-06-15 16:50:38 +0545

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
-function App() {
+const App = () => {
+const [file,setFile]=useState([])
+  useEffect(()=>{
+    axios.get('https://jsonplaceholder.typicode.com/posts')
+    .then((res)=>{console.log("getting from ::", res.data)
+    setFile(res.data)})
+    .catch((err=>{console.log(err)}))
+
+
+  },[ ])
+
+  const arr = file.map((data,i)=>{
+    return(
+      <tr key={i}>
+<td>{data.id}</td>
+<td>{data.title}</td>
+<td>{data.body}</td>
+</tr>
+
+    )
+  })
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>Getting Data from API using axios.</p>
+      <table border={1}>
+<tr>
+<th>ID</th>
+<th>Title</th>
+<th>Body</th>
+</tr>
+{arr.slice(0,10)}
+
+</table>
+      
+
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
